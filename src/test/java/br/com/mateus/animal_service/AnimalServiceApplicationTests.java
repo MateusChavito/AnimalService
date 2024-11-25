79fe8a58204b90f45c37a428cd6d2db06cdb6f53
MateusChavito <mateus.chavito@hotmail.com>
2024-11-25 18:35:14 -0300

    Subindo projeto

---
+++ src/test/java/br/com/mateus/animal_service/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateus.animal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

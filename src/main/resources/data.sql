806916ca58797bdd09938ed92b847e9374372ea3
MateusChavito <mateus.chavito@hotmail.com>
2024-11-26 15:41:37 -0300

    Inserindo tabelas e dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Cachorrinho da Ebac', 5, 'SRD', current_date, 'Bem pulguento', 'Cleber', 'Pequeno');
+
+INSERT
+INTO
+  animal
+  (nome_provisorio, idade_estimada, raca, data_entrada, condicoes_chegada, nome_recebedor, porte)
+VALUES
+  ('Outro cachorrinho da Ebac', 10, 'Rottweiler', current_date, 'Bem velhinho', 'Renato', 'Grande');
